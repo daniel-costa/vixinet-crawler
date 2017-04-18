06a762c499732fb773242cc0afc817b1495856e5
Daniel Costa <costa@vixinet.ch>
2017-04-18 14:01:48 +0300

    delete logs

--- app/jobs/harvest_entry_job.rb
+++ app/jobs/harvest_entry_job.rb
@@ -10,14 +10,11 @@ class HarvestEntryJob < ApplicationJob
       return nil
     end
 
-    puts "here"
-
     # Harvest similar pages
     crops.each do |crop|
-      puts "#{entry.platform.base_url}#{crop}"
       new_entry = Entry.find_or_initialize_by(url: "#{entry.platform.base_url}#{crop}", platform: entry.platform)
       if new_entry.save()
-      	# ToDo Avoid crawling if it's not a new entry
+        # ToDo Avoid crawling if it's not a new entry
         CrawlEntryJob.perform_later(new_entry.id.to_s)
       end
     end
